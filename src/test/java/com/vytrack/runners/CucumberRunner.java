5f584dd19a096a89f9ded5b07aa28748d54a2b49
IssB20 <islem_ouiddir@yahoo.fr>
2020-11-12 20:34:01 -0500

    delete cradle package

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -7,13 +7,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {
-          "rerun:target/rerun.txt"
-        },
 
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = "@negative_login",
         publish = true
 )
